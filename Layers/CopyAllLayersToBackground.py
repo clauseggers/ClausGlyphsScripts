1d2dd7e8c0f60c115a543b67bddb613f74ca0d03
Claus Eggers Sørensen <es@forthehearts.net>
2023-02-07 12:06:52 +0100

    Added a copy all layers to background

---
+++ Layers/CopyAllLayersToBackground.py
@@ -0,0 +1,14 @@
+#MenuTitle: Copies all the layers of the glyph to their backgrounds.
+#Created by Claus
+# -*- coding: utf-8 -*-
+__doc__="""
+Copies all the layers of the current glyph to their backgrounds.
+"""
+
+for layer in Glyphs.font.selectedLayers:
+	# Access to the glyph 
+	glyph = layer.parent
+	# Access to all layer of the glyph
+	for layer in glyph.layers:
+		# Copy layer to its background
+		layer.background = layer.copy()

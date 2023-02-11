5ddd277c2e456528ab5210b357b2718bd4f69fe4
Claus Eggers Sørensen <es@forthehearts.net>
2023-02-11 18:09:25 +0100

    Added a script freeing vertical alignment

---
+++ Layers/FreeVerticalAlgnment.py
@@ -0,0 +1,16 @@
+#MenuTitle: Free the Vertical Alignment
+#Created by Claus
+# -*- coding: utf-8 -*-
+__doc__="""
+Frees the vertical alignment, but preserves the horizontal.
+"""
+
+for layer in Glyphs.font.selectedLayers:
+	# Access to the glyph
+	glyph = layer.parent
+	# Access to all layer of the glyph
+	for layer in glyph.layers:
+		# Copy layer to its background
+		layer.shapes[0].alignment = 3
+
+# EOF

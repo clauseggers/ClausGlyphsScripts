e18876bf0a9003f35b874599f6d0d35efa2c6629
Claus Eggers Sørensen <es@forthehearts.net>
2023-02-27 16:39:46 +0100

    Added shift components by value script

--- Spacing/SetSidebearingsTo30.py
+++ Spacing/SetSidebearingsTo30.py
@@ -1,14 +1,14 @@
-#MenuTitle: Sets sidebearings of all layers of current glyph to 30
+#MenuTitle: Sets side-bearings of all layers of current glyph to 30
 #Created by Claus
 # -*- coding: utf-8 -*-
 __doc__="""
-Sets the sidebearings of all the layers of the current glyph to 30.
+Sets the side-bearings of all the layers of the current glyph to 30.
 """
 
 for layer in Glyphs.font.selectedLayers:
-	# Access to the glyph 
+	# Access to the glyph
 	glyph = layer.parent
-	# Access to all layer of the glyph
+	# Access to all layers of the glyph
 	for layer in glyph.layers:
 		layer.RSB = 30
 		layer.LSB = 30

362e60d86e1e7c278300fbbf751bc07bc2a11761
Claus Eggers Sørensen <es@forthehearts.net>
2023-07-29 11:02:36 +0200

    Added Enable Overlap Preview script

---
+++ Corner & Smart components/EnableRemoveOverlapPreview.py
@@ -0,0 +1,10 @@
+#MenuTitle: Enable Remove Overlap preview
+#Created by Claus
+# -*- coding: utf-8 -*-
+__doc__="""
+Enables the preview of Remove Overlap.
+"""
+
+Font.previewRemoveOverlap = True
+
+# EOF

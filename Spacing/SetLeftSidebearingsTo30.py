cb557ea45c5dfb26cacbc900328c59c020ade66d
Claus Eggers Sørensen <es@forthehearts.net>
2024-01-23 10:20:27 +0100

    Added left and right sidebearing scripts

---
+++ Spacing/SetLeftSidebearingsTo30.py
@@ -0,0 +1,15 @@
+#MenuTitle: Sets side-bearings of all layers of current glyph to 30
+#Created by Claus
+# -*- coding: utf-8 -*-
+__doc__="""
+Sets the side-bearings of all the layers of the current glyph to 30.
+"""
+
+for layer in Glyphs.font.selectedLayers:
+	# Access to the glyph
+	glyph = layer.parent
+	# Access to all layers of the glyph
+	for layer in glyph.layers:
+		layer.LSB = 30
+
+# EOF

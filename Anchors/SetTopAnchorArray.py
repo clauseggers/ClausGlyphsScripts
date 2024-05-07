7603a06d1fdac7afe3ac8c0d740062871c67ccb9
Claus Eggers Sørensen <es@forthehearts.net>
2024-05-07 07:14:58 +0200

    Working version of the unified script

--- Anchors/SetTopAnchorArray.py
+++ Anchors/SetTopAnchorArray.py
@@ -1,8 +1,8 @@
-#MenuTitle: Interactively set a vertical array of `top` anchors
+#MenuTitle: Interactively sets a vertical array of `to_low_N` anchors aligned with an existing anchor
 #Created by Claus
 # -*- coding: utf-8 -*-
 __doc__="""
-Interactively set a vertical array of `top_low_N` anchors using the existing `top` or `_top` anchor for alignment.
+Interactively sets a vertical array of `top_low_N` anchors using an existing anchor for alignment.
 """
 
 from GlyphsApp import *
@@ -15,13 +15,15 @@ font.disableUpdateInterface()
 
 class AnchorDialog:
     def __init__(self):
-        self.w = FloatingWindow((300, 230), "Anchor Settings")
+        self.w = FloatingWindow((300, 270), "Anchor Settings")
         self.w.num_anchors_text = TextBox((10, 10, -10, 20), "Number of 'top_low_*' anchors")
         self.w.num_anchors_input = EditText((10, 35, -10, 20), placeholder="3")
         self.w.spacing_text = TextBox((10, 70, -10, 20), "Spacing between anchors")
         self.w.spacing_input = EditText((10, 95, -10, 20), placeholder="20")
         self.w.delete_existing_anchors = CheckBox((10, 130, -10, 20), "Delete existing 'top_low_*' anchors", value=False)
-        self.w.ok_button = Button((10, 160, -10, 20), "Enter", callback=self.ok_callback)
+        self.w.anchor_choice_text = TextBox((10, 165, -10, 20), "Anchor to align to:")
+        self.w.anchor_choice_input = PopUpButton((10, 190, -10, 20), ["top", "_top"], callback=self.anchor_choice_callback)
+        self.w.ok_button = Button((10, 220, -10, 20), "Enter", callback=self.ok_callback)
         self.w.bind("resize", self.resize_window)
         self.w.open()
 
@@ -31,21 +33,28 @@ class AnchorDialog:
         self.w.spacing_text.setPosSize((10, 70, -10, 20))
         self.w.spacing_input.setPosSize((10, 95, -10, 20))
         self.w.delete_existing_anchors.setPosSize((10, 130, -10, 20))
-        self.w.ok_button.setPosSize((10, 160, -10, 20))
+        self.w.anchor_choice_text.setPosSize((10, 165, -10, 20))
+        self.w.anchor_choice_input.setPosSize((10, 190, -10, 20))
+        self.w.ok_button.setPosSize((10, 220, -10, 20))
+
+    def anchor_choice_callback(self, sender):
+        pass
 
     def ok_callback(self, sender):
         num_anchors = int(self.w.num_anchors_input.get())
         spacing = int(self.w.spacing_input.get())
         delete_existing_anchors = self.w.delete_existing_anchors.get()
+        anchor_choice = self.w.anchor_choice_input.getItems()[self.w.anchor_choice_input.get()]
         print("Number of anchors:", num_anchors)
         print("Spacing between anchors:", spacing)
         print("Delete existing anchors:", delete_existing_anchors)
+        print("Anchor choice:", anchor_choice)
         self.w.close()
-        create_anchors(num_anchors, spacing, delete_existing_anchors)
+        create_anchors(num_anchors, spacing, delete_existing_anchors, anchor_choice)
 
-def create_anchors(num_anchors, spacing, delete_existing_anchors):
+def create_anchors(num_anchors, spacing, delete_existing_anchors, anchor_choice):
     # Define anchor names
-    anchor_names = ["top"] + ["top_low_" + str(i) for i in range(1, num_anchors+1)]
+    anchor_names = [anchor_choice] + ["top_low_" + str(i) for i in range(1, num_anchors+1)]
 
     selected_layers = font.selectedLayers
 
@@ -65,16 +74,16 @@ def create_anchors(num_anchors, spacing, delete_existing_anchors):
                 for anchor in existing_low_anchors:
                     layer.removeAnchor_(anchor)
 
-            existing_anchor = layer.anchors["top"]
+            existing_anchor = layer.anchors[anchor_choice]
             if existing_anchor:
                 x_coordinate = existing_anchor.position.x
                 y_coordinate = existing_anchor.position.y
                 for anchor_name in anchor_names:
-                    if anchor_name != "top":
+                    if anchor_name != anchor_choice:
                         y_coordinate -= spacing
                     layer.anchors.append(GSAnchor(anchor_name, NSPoint(x_coordinate, y_coordinate)))
             else:
-                print("No 'top' anchor found in layer:", layer.name)
+                print("No '{}' anchor found in layer: {}".format(anchor_choice, layer.name))
     print("Anchors creation complete")
 
 AnchorDialog()
